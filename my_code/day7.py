76eb0ff60751c6d7b6a7d072c8624f03afc573d8
wpersmile <775016131@qq.com>
2020-07-13 23:51:05 +0800

    最后一个单词的长度代码

--- my_code/day7.py
+++ my_code/day7.py
@@ -5,6 +5,7 @@
 # @Subject : https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
 
 
+# 逻辑常规判断
 def lengthOfLastWord(s):
     s = s.rstrip()
     s_len = len(s)
@@ -18,6 +19,21 @@ def lengthOfLastWord(s):
     return len(last_word)
 
 
+# 链表实现
+def lengthOfLastWord2(s):
+    s_list = list(s)
+    last_word_list = []
+    while len(s_list) != 0:
+        s_pop = s_list.pop()
+        if s_pop != ' ':
+            last_word_list.insert(0, s_pop)
+        else:
+            if len(last_word_list) != 0:
+                break
+    last_word = ''.join(last_word_list)
+    return len(last_word)
+
+
 if __name__ == '__main__':
-    test_s = ' 2    33'
-    print(lengthOfLastWord(test_s))
+    test_s = ' 33   2  '
+    print(lengthOfLastWord2(test_s))
