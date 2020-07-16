f615b9ec47b45754d7ba7be693ec5525f7b4f24d
wpersmile <775016131@qq.com>
2020-07-16 23:55:38 +0800

    相交链表代码

---
+++ my_code/day10.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/16 23:54
+# @Author  : wperSmile
+# @Subject : https://leetcode-cn.com/problems/intersection-of-two-linked-lists/submissions/
+
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        headA_len = 0
+        headB_len = 0
+        p = headA
+        if headA == None or headB == None:
+            return None
+
+        while p != None:
+            p = p.next
+            headA_len = headA_len + 1
+        p = headB
+        while p != None:
+            p = p.next
+            headB_len = headB_len + 1
+        value = 0
+        if headA_len >= headB_len:
+            value = headA_len - headB_len
+            pa = headA
+            pb = headB
+            for i in range(0, value):
+                pa = pa.next
+            while pa.next != None:
+                if pa != pb:
+                    pa = pa.next
+                    pb = pb.next
+                else:
+                    return pa
+        else:
+            value = headB_len - headA_len
+            pa = headA
+            pb = headB
+            for i in range(0, value):
+                pb = pb.next
+            while pa.next != None:
+                if pa != pb:
+                    pa = pa.next
+                    pb = pb.next
+                else:
+                    return pa
+        return None
