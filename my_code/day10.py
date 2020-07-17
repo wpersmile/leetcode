0296ccfba4f41b78a9aca35de5f007ab60e734b7
wpersmile <775016131@qq.com>
2020-07-17 22:04:37 +0800

    相交链表代码

--- my_code/day10.py
+++ my_code/day10.py
@@ -5,27 +5,30 @@
 # @Subject : https://leetcode-cn.com/problems/intersection-of-two-linked-lists/submissions/
 
 
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-#
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         headA_len = 0
         headB_len = 0
-        p = headA
+
         if headA == None or headB == None:
             return None
 
+        p = headA
         while p != None:
             p = p.next
             headA_len = headA_len + 1
+
         p = headB
         while p != None:
             p = p.next
             headB_len = headB_len + 1
+
         value = 0
         if headA_len >= headB_len:
             value = headA_len - headB_len
@@ -39,6 +42,8 @@ class Solution:
                     pb = pb.next
                 else:
                     return pa
+            if pa == pb:
+                return pa
         else:
             value = headB_len - headA_len
             pa = headA
@@ -50,5 +55,8 @@ class Solution:
                     pa = pa.next
                     pb = pb.next
                 else:
+                    return pa
+            if pa.next == None:
+                if pa == pb:
                     return pa
         return None
